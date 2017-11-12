7350e483f5ff36aa5e0a29a0c84945d3e5185ae3
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2017-11-12 22:22:31 +0100

    Add regression tests

---
+++ tests/test_quadrature_analysis.py
@@ -0,0 +1,24 @@
+import os
+
+from Quadrature_Analysis import main
+
+def test_main_function_with_correct_arguments():
+    assert main('f1', '1', '1', '10') == None
+
+def test_errors_produced_by_f1():
+    main('f1', '1', '1', '10')
+
+    errors = """\
+1 0.596153846154
+2 0.136752136752
+3 0.00923076923077
+4 4.06325614181e-17
+5 1.18745157937e-16
+6 2.01674114107e-16
+7 1.12002254426e-16
+8 4.92144386141e-17
+9 2.63586228165e-17
+10 7.8813157923e-19
+"""
+    with open(os.path.join('errors', 'f1.txt')) as error_file:
+        assert error_file.read() == errors
