890c81270a7b838768267d8fa1453dc347c722da
Mikal Stapnes <mstapnes@gmail.com>
2017-11-17 17:07:04 +0100

    Example code

--- Area_Integral.py
+++ Area_Integral.py
@@ -1,16 +1,37 @@
 ################################################################################
+import sys
 import numpy as np
 import scipy as sp
-import splipy as spl
-from splipy.io import *
+
+import Spline_Quadrature as SPQ
+from splipy.io import G2
 import splipy.surface_factory as spf
 ################################################################################
 
 
-def Prepare_Data(T,p):
 
-def Assembly(basis,I,W,X,n):
 
-def Spline_Integral(T,p):
 
-def Area_Integral():
+def Area_Integral(path):
+    with G2(path) as file:
+        pass
+
+        return
+
+    
+
+def main(*args, **kwargs):
+    
+    args = args if args else sys.argv[1:]
+    
+    if not args:
+        path = "Curve.g2"
+    else:    
+        path = args[0]
+    
+    print(Area_Integral(path))
+    
+    
+    
+if __name__ == "__main__":
+    main()
