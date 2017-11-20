85eb32e6a0481b3091a0ce778c3013cb871b94d4
Mikal Stapnes <mstapnes@gmail.com>
2017-11-20 16:02:21 +0100

    Small style changes

--- Curve_Plot.py
+++ Curve_Plot.py
@@ -3,42 +3,31 @@ import numpy as np
 #import splipy as spl
 from splipy.io import G2
 import matplotlib.pyplot as plt
-from matplotlib.collections import LineCollection
 #import sys
 ################################################################################
 
 
 def Curve_Plot():
     with G2('Curve.g2') as file:
-        curve = file.read()[0]
+        curve = (file.read()[0])
         
-        N = 150
+        N = 180
         t = np.linspace(curve.start(), curve.end(), N)        
+        
+        print("Start: {}   End: {}".format(curve.start(), curve.end()))
+        
         V = curve.evaluate(t)
         X = V[:, 0]
         Y = V[:, 1]
-
-        # set up a list of (x,y) points
-        points = np.array([X,Y]).transpose().reshape(-1,1,2)
         
-        # set up a list of segments
-        segs = np.concatenate([points[:-1],points[1:]],axis=1)
-
-        # make the collection of segments
-        lc = LineCollection(segs, cmap=plt.get_cmap('jet'))
-        lc.set_array(t) # color the segments by our parameter
-        
-        # plot the collection
-        plt.gca().add_collection(lc)
-        plt.xlim(X.min(), X.max())
-        plt.ylim(Y.min(), Y.max())
-        plt.colorbar(lc, label="$t$")
-        
-        plt.axis('equal')
-        
+        plt.figure()
+        plt.gcf().subplots_adjust(left=0.18)
+        plt.plot(X, Y)
         plt.xlabel("$x$")
-        plt.ylabel("$y$")
-
+        plt.ylabel("$y$")#, rotation='horizontal')
+        plt.savefig("Curve_Plot.pdf")
         plt.show()
         
-Curve_Plot()
+
+if __name__ == "__main__":    
+    Curve_Plot()
